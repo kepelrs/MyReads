141f91188aef67f4b1ff03814b93857af98286e6
Davi Barreto <davivbarreto@gmail.com>
2018-07-23 01:43:06 +0300

    Fetch books from BooksApi and pass as props to MainPage

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React from 'react'
 import { Route } from "react-router-dom";
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    allBooks: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((allBooks) => {
+      this.setState({ allBooks })
+      console.log(allBooks)
+    }).then(console.log(this.state.allBooks))
+  }
 
   render() {
     return (
       <div className="app">
-      <Route exact path="/" component={MainPage}/>
+      <Route exact path="/" render={()=> (
+        <MainPage
+          currentlyReading={this.state.allBooks.filter(book=>book.shelf === "currentlyReading")}
+          wantToRead={this.state.allBooks.filter(book=>book.shelf === "wantToRead")}
+          read={this.state.allBooks.filter(book=>book.shelf === "read")}
+        />)}
+      />
       <Route path="/search" component={SearchPage}/>
       </div>
     )
