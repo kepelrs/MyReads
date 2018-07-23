a73bc5e39d01f42232387f572bb728122fc550ac
Davi Barreto <davivbarreto@gmail.com>
2018-07-23 05:25:28 +0300

    Initial working setup for SearchPage.
    Todo:
    1. Handle undefined variables and errors
    2. Refactor the code
    3. Possibly create a book component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class BooksApp extends React.Component {
   state = {
     currentlyReading: [],
     wantToRead: [],
-    read: []
+    read: [],
+    queryResults: []
   }
 
   componentDidMount() {
@@ -30,15 +31,20 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-      <Route exact path="/" render={()=> (
-        <MainPage
-          currentlyReading={this.state.currentlyReading}
-          wantToRead={this.state.wantToRead}
-          read={this.state.read}
-          changeShelf={(book, shelf)=>BooksAPI.update(book, shelf).then(this.refreshStateData)}
-        />)}
-      />
-      <Route path="/search" component={SearchPage}/>
+        <Route exact path="/" render={()=> (
+          <MainPage
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read}
+            changeShelf={(book, shelf)=>BooksAPI.update(book, shelf).then(this.refreshStateData)}
+          />)}
+        />
+        <Route path="/search" render={()=> (
+          <SearchPage
+            searchApi={(query)=>BooksAPI.search(query).then(results=>this.setState({queryResults: results || []}))}
+            bookMatches={this.state.queryResults}
+          />)}
+        />
       </div>
     )
   }
