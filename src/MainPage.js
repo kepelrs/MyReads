6bb3483c7256ed11da12e02cad7c4a4ce90b9e70
Davi Barreto <davivbarreto@gmail.com>
2018-07-23 16:20:10 +0300

    Minor refactor to Main and Search pages

--- src/MainPage.js
+++ src/MainPage.js
@@ -6,34 +6,31 @@ class MainPage extends Component{
   render() {
     return (
       <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
-      <div className="list-books-content">
-        <div>
-        <Shelf
-            title="Currently Reading"
-            books={this.props.currentlyReading}
-            defaultSelection="currentlyReading"
-            changeShelf={this.props.changeShelf}
-          />
-          <Shelf
-            title="Want to Read"
-            books={this.props.wantToRead}
-            defaultSelection="wantToRead"
-            changeShelf={this.props.changeShelf}
-          />
-          <Shelf
-            title="Read"
-            books={this.props.read}
-            defaultSelection="read"
-            changeShelf={this.props.changeShelf}
-          />
+        <div className="list-books-title">
+          <h1>MyReads</h1>
         </div>
-      </div>
-      <div className="open-search">
-        <Link to="/search">Add a book</Link>
-      </div>
+        <div className="list-books-content">
+          <div>
+            <Shelf
+              title="Currently Reading"
+              books={this.props.currentlyReading}
+              changeShelf={this.props.changeShelf}
+            />
+            <Shelf
+              title="Want to Read"
+              books={this.props.wantToRead}
+              changeShelf={this.props.changeShelf}
+            />
+            <Shelf
+              title="Read"
+              books={this.props.read}
+              changeShelf={this.props.changeShelf}
+            />
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
       </div>
     )
   }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom'
 import Book from './Book';
 
 class SearchPage extends Component{
-  findBooks = (event=false) => {
-    const query = event ? event.target.value : ''
+  findBooks = (event) => {
+    const query = event && event.target.value
     this.props.queryApi(query)
   }
 
   render() {
     return (
       <div className="search-books">
-      <div className="search-books-bar">
-        <Link className="close-search" to="/" onClick={()=>this.findBooks()}>Close</Link>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" onChange={this.findBooks}/>
+        <div className="search-books-bar">
+          <Link className="close-search" to="/" onClick={()=>this.findBooks()}>Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" onChange={this.findBooks}/>
+          </div>
         </div>
-      </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {this.props.bookMatches.map(matchedBook=>(
-            <Book key={matchedBook.id} book={matchedBook} changeShelf={this.props.changeShelf}/>
-          ))}
-        </ol>
-      </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.props.bookMatches.map(matchedBook=>(
+              <Book key={matchedBook.id} book={matchedBook} changeShelf={this.props.changeShelf}/>
+            ))}
+          </ol>
+        </div>
       </div>
     )
   }
