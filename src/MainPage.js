6cec34be039b82f770725ac5969169663226eba4
Davi Barreto <davivbarreto@gmail.com>
2018-07-23 03:12:04 +0300

    Refactor: Create Shelf component

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import Shelf from './Shelf';
 
 class MainPage extends Component{
   render() {
@@ -10,93 +11,21 @@ class MainPage extends Component{
       </div>
       <div className="list-books-content">
         <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.currentlyReading.map(book=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 190,backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                        <div className="book-shelf-changer">
-                          <select defaultValue="currentlyReading">
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      {book.authors.map((author, index)=>(
-                        <div key={index} className="book-authors">{author}</div>
-                      ))}
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.wantToRead.map(book=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 190,backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                        <div className="book-shelf-changer">
-                          <select defaultValue="wantToRead">
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      {book.authors.map((author, index)=>(
-                        <div key={index} className="book-authors">{author}</div>
-                      ))}
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {this.props.read.map(book=>(
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 190,backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-                        <div className="book-shelf-changer">
-                          <select defaultValue="read">
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      {book.authors.map((author, index)=>(
-                        <div key={index} className="book-authors">{author}</div>
-                      ))}
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
+        <Shelf
+            title="Currently Reading"
+            books={this.props.currentlyReading}
+            defaultSelection="currentlyReading"
+          />
+          <Shelf
+            title="Want to Read"
+            books={this.props.wantToRead}
+            defaultSelection="wantToRead"
+          />
+          <Shelf
+            title="Read"
+            books={this.props.read}
+            defaultSelection="read"
+          />
         </div>
       </div>
       <div className="open-search">
---
+++ src/Shelf.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Shelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map(book=>(
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 190,backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                    <div className="book-shelf-changer">
+                      <select defaultValue={this.props.defaultSelection}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  {book.authors.map((author, index)=>(
+                    <div key={index} className="book-authors">{author}</div>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf;
